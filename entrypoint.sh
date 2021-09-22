459635cd0571bbdf2c9b138db5ae3ea799ac672e
OsirisInferi <github@flut.demon.nl>
2021-09-22 20:24:10 +0200

    Add some moar

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,25 @@ export PORTDIR_OVERLAY="."
 export ACCEPT_KEYWORDS="~amd64"
 export FEATURES="-ipc-sandbox -network-sandbox"
 
-find var/cache/
+find /var/cache/binpkgs
+find /var/cache/distfiles
 
-find /var/cache/
+emerge -qvbk app-portage/gentoolkit
 
-emerge --info
+emerge -qvbk --buildpkg-exclude "*/*::certbot-dns-plugins" \
+	app-crypt/certbot-dns-cloudflare \
+	app-crypt/certbot-dns-cloudxn \
+	app-crypt/certbot-dns-digitalocean \
+	app-crypt/certbot-dns-dnsimple \
+	app-crypt/certbot-dns-dnsmadeeasy \
+	app-crypt/certbot-dns-gehirn \
+	app-crypt/certbot-dns-google \
+	app-crypt/certbot-dns-linode \
+	app-crypt/certbot-dns-luadns \
+	app-crypt/certbot-dns-ovh \
+	app-crypt/certbot-dns-rfc2136 \
+	app-crypt/certbot-dns-route53 \
+	app-crypt/certbot-dns-sakuracloud
 
-emerge -qvbk --buildpkg-exclude "*/*::certbot-dns-plugins" certbot-dns-cloudflare certbot-dns-cloudxns certbot-dns-digitalocean certbot-dns-dnsimple certbot-dns-dnsmadeeasy certbot-dns-gehirn certbot-dns-google certbot-dns-linode certbot-dns-luadns certbot-dns-ovh certbot-dns-rfc2136 certbot-dns-route53 certbot-dns-sakuracloud
+eclean packages
+eclean distfiles
