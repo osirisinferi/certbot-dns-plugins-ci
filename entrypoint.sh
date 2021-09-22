e07d6869c1e95cbd2ea3166392cdc816e3b099aa
OsirisInferi <github@flut.demon.nl>
2021-09-22 14:59:10 +0200

    Add -b and -k options so we can use Github cache

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ export PORTDIR_OVERLAY="."
 export ACCEPT_KEYWORDS="~amd64"
 export FEATURES="-ipc-sandbox -network-sandbox"
 
-emerge -qv certbot-dns-cloudflare certbot-dns-cloudxns certbot-dns-digitalocean certbot-dns-dnsimple certbot-dns-dnsmadeeasy certbot-dns-gehirn certbot-dns-google certbot-dns-linode certbot-dns-luadns certbot-dns-ovh certbot-dns-rfc2136 certbot-dns-route53 certbot-dns-sakuracloud
+emerge -qvbk --buildpkg-exclude "*/*::certbot-dns-plugins" certbot-dns-cloudflare certbot-dns-cloudxns certbot-dns-digitalocean certbot-dns-dnsimple certbot-dns-dnsmadeeasy certbot-dns-gehirn certbot-dns-google certbot-dns-linode certbot-dns-luadns certbot-dns-ovh certbot-dns-rfc2136 certbot-dns-route53 certbot-dns-sakuracloud
