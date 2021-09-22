50dc38bb33d71e17f0523acc116e3f883a6964b8
OsirisInferi <github@flut.demon.nl>
2021-09-22 20:01:31 +0200

    Moar debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,10 @@ export PORTDIR_OVERLAY="."
 export ACCEPT_KEYWORDS="~amd64"
 export FEATURES="-ipc-sandbox -network-sandbox"
 
-find var/cache/binpkgs
+find var/cache/
 
-find var/cache/distfiles
+find /var/cache/
+
+emerge --info
 
 emerge -qvbk --buildpkg-exclude "*/*::certbot-dns-plugins" certbot-dns-cloudflare certbot-dns-cloudxns certbot-dns-digitalocean certbot-dns-dnsimple certbot-dns-dnsmadeeasy certbot-dns-gehirn certbot-dns-google certbot-dns-linode certbot-dns-luadns certbot-dns-ovh certbot-dns-rfc2136 certbot-dns-route53 certbot-dns-sakuracloud
