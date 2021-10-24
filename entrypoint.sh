770f20691728864eb7f06b19b39ddda5d2c79047
osirisinferi <github@flut.demon.nl>
2021-10-24 22:55:41 +0200

    Implement working distfiles/binpkgs cache

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,36 @@ export PORTDIR_OVERLAY="."
 export ACCEPT_KEYWORDS="~amd64"
 export FEATURES="-ipc-sandbox -network-sandbox"
 
-emerge -qvbk --buildpkg-exclude "*/*::certbot-dns-plugins" certbot-dns-cloudflare certbot-dns-cloudxns certbot-dns-digitalocean certbot-dns-dnsimple certbot-dns-dnsmadeeasy certbot-dns-gehirn certbot-dns-google certbot-dns-linode certbot-dns-luadns certbot-dns-ovh certbot-dns-rfc2136 certbot-dns-route53 certbot-dns-sakuracloud
+mkdir -p $(pwd)/var/cache/{binpkgs,distfiles}
+
+export PKGDIR="$(pwd)/var/cache/binpkgs"
+export DISTDIR="$(pwd)/var/cache/distfiles"
+
+emerge -qvbk app-portage/gentoolkit
+
+emerge -qvbk --buildpkg-exclude "*/*::certbot-dns-plugins" \
+	app-crypt/certbot-dns-cloudflare \
+	app-crypt/certbot-dns-cloudxns \
+	app-crypt/certbot-dns-digitalocean \
+	app-crypt/certbot-dns-dnsimple \
+	app-crypt/certbot-dns-dnsmadeeasy \
+	app-crypt/certbot-dns-gehirn \
+	app-crypt/certbot-dns-google \
+	app-crypt/certbot-dns-linode \
+	app-crypt/certbot-dns-luadns \
+	app-crypt/certbot-dns-ovh \
+	app-crypt/certbot-dns-rfc2136 \
+	app-crypt/certbot-dns-route53 \
+	app-crypt/certbot-dns-sakuracloud
+
+emerge_status=$?
+
+eclean packages
+packages_status=$?
+
+eclean distfiles
+distfiles_status=$?
+
+status=$(expr $emerge_status + $packages_status + $distfiles_status)
+[ $status -eq 0 ] && exit 0 || exit 1
+
