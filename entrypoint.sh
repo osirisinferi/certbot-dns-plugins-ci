ee51b984046b821b3fe4e970c1b52cf1be764509
osirisinferi <github@flut.nl.eu.org>
2021-12-30 21:31:06 +0100

    Remove first party plugins and change to third party plugin(s)

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,19 +11,7 @@ export DISTDIR="$(pwd)/var/cache/distfiles"
 emerge -qvbk app-portage/gentoolkit
 
 emerge -qvbk --buildpkg-exclude "*/*::certbot-dns-plugins" \
-	app-crypt/certbot-dns-cloudflare \
-	app-crypt/certbot-dns-cloudxns \
-	app-crypt/certbot-dns-digitalocean \
-	app-crypt/certbot-dns-dnsimple \
-	app-crypt/certbot-dns-dnsmadeeasy \
-	app-crypt/certbot-dns-gehirn \
-	app-crypt/certbot-dns-google \
-	app-crypt/certbot-dns-linode \
-	app-crypt/certbot-dns-luadns \
-	app-crypt/certbot-dns-ovh \
-	app-crypt/certbot-dns-rfc2136 \
-	app-crypt/certbot-dns-route53 \
-	app-crypt/certbot-dns-sakuracloud
+	app-crypt/certbot-dns-freenom
 
 emerge_status=$?
 
