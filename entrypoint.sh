0ee41ee8554d7c7262507df871bf243bfdcb6327
osirisinferi <github@flut.nl.eu.org>
2022-01-02 22:50:52 +0100

    Dynamically load lists of plugins from overlay

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,22 +10,18 @@ export DISTDIR="$(pwd)/var/cache/distfiles"
 
 emerge -qvbk app-portage/gentoolkit
 
-emerge -qvbk --buildpkg-exclude "*/*::certbot-dns-plugins" \
-	app-crypt/certbot-dns-cloudflare \
-	app-crypt/certbot-dns-cloudxns \
-	app-crypt/certbot-dns-digitalocean \
-	app-crypt/certbot-dns-dnsimple \
-	app-crypt/certbot-dns-dnsmadeeasy \
-	app-crypt/certbot-dns-gehirn \
-	app-crypt/certbot-dns-google \
-	app-crypt/certbot-dns-linode \
-	app-crypt/certbot-dns-luadns \
-	app-crypt/certbot-dns-ovh \
-	app-crypt/certbot-dns-rfc2136 \
-	app-crypt/certbot-dns-route53 \
-	app-crypt/certbot-dns-sakuracloud
+plugins_full=($(equery -q list -o -F '=$cpv' "certbot-dns-*"))
 
-emerge_status=$?
+emerge_status=0
+
+for plugin in "${plugins_full[@]}"
+do
+        emerge -qvbk --buildpkg-exclude "*/*::certbot-dns-plugins */*::third-party-certbot-dns-plugins" ${plugin}
+        emerge_status=$?
+        if [ ${emerge_status} -ne 0 ]; then
+                break
+        fi
+done
 
 eclean packages
 packages_status=$?
@@ -56,19 +52,7 @@ function check_plugins {
 
 loaded_plugins=($(certbot plugins 2>&1 | pcregrep -o1 "^\* dns-(.+)"))
 
-expected_plugins=(cloudflare \
-cloudxns \
-digitalocean \
-dnsimple \
-dnsmadeeasy \
-gehirn \
-google \
-linode \
-luadns \
-ovh \
-rfc2136 \
-route53 \
-sakuracloud)
+expected_plugins=($(equery -q list -o -F '$name' "certbot-dns-*" | sort -u | sed 's/certbot-dns-//'))
 
 check_plugins loaded_plugins expected_plugins
 check_plugins_status=$?
