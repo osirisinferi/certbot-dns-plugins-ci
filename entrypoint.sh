0bb9a4277be6221145e2cb976f128d85ed0f8202
osirisinferi <github@flut.nl.eu.org>
2022-01-02 18:19:42 +0100

    Add `certbot plugins` check to CI

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,6 +33,46 @@ packages_status=$?
 eclean distfiles
 distfiles_status=$?
 
-status=$(expr $emerge_status + $packages_status + $distfiles_status)
+function check_plugins {
+        local -n _loaded_plugins=$1
+        local -n _expected_plugins=$2
+        IFS=$'\n' local loaded_plugins_sorted=($(sort <<<"${_loaded_plugins[*]}"))
+        unset IFS
+        IFS=$'\n' local expected_plugins_sorted=($(sort <<<"${_expected_plugins[*]}"))
+        unset IFS
+
+        diff=$(diff <(printf "%s\n" "${loaded_plugins_sorted[@]}") <(printf "%s\n" "${expected_plugins_sorted[@]}"))
+
+        if [[ -z "$diff" ]]; then
+                echo "All good."
+                return 0
+        else
+                echo "Plugin difference!"
+                echo "< loaded plugins vs. > expected plugins:"
+                echo ${diff}
+                return 1
+        fi
+}
+
+loaded_plugins=($(certbot plugins 2>&1 | pcregrep -o1 "^\* dns-(.+)"))
+
+expected_plugins=(cloudflare \
+cloudxns \
+digitalocean \
+dnsimple \
+dnsmadeeasy \
+gehirn \
+google \
+linode \
+luadns \
+ovh \
+rfc2136 \
+route53 \
+sakuracloud)
+
+check_plugins loaded_plugins expected_plugins
+check_plugins_status=$?
+
+status=$(expr $emerge_status + $packages_status + $distfiles_status + $check_plugins_status)
 [ $status -eq 0 ] && exit 0 || exit 1
 
